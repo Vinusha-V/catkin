24dc3712c1726fa944dd71fe5a449f7e2813319a
Vinusha v <bene20108.ad@rmkec.ac.in>
2023-09-01 11:14:27 +0530

    final

--- maps.py
+++ maps.py
@@ -1,41 +1,52 @@
 import open3d as o3d
 import json
-from geonav_conversions import xy2ll
+from geonav_conversions import xy2ll 
+import yaml
 
-def pcd_to_geojson(input_pcd, output_geojson, orglat, orglon):
+def read_config(filename):
+    with open(filename, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def pcd_to_geojson(input_pcd, output_geojson, config_file):
+    config = read_config(config_file)
+    orglat = config.get('map_origin', {}).get('latitude', 0.0)
+    orglon = config.get('map_origin', {}).get('longitude', 0.0)
     pcd = o3d.io.read_point_cloud(input_pcd)
     points = pcd.points
-    coordinates = []
+
+    features = [] 
+    
     for point in points:
         x, y, _ = point
-        lon, lat = xy2ll(x, y, orglat, orglon)
-        coordinates.append([lon, lat])
+        lat, lon = xy2ll(x, y, orglat, orglon)
+
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [lon, lat]
+            },
+            "properties": {
+                "marker-color": "#ff8000",  
+                "marker-size": "small",     
+                "marker-symbol": "star",    
+            }
+        }
+
+        features.append(feature)
 
     geojson_data = {
         "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {
-                    "Name": "demo" 
-                },
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [coordinates]
-                },
-                "id": 0
-            }
-        ]
+        "features": features
     }
-
+    
     with open(output_geojson, "w") as geojson_file:
         json.dump(geojson_data, geojson_file, indent=2)
 
 if __name__ == "__main__":
-    input_pcd = "/home/vinusha/Documents/maps/pointcloud_map.pcd"  
-    output_geojson = "output1.geojson"  
-    orglat = 35.23808753540768  
-    orglon = 139.9009591876285  
+    input_pcd = "varma_clean_tree_map.pcd" 
+    output_geojson = "output.geojson" 
+    config_file = "varma_clean_map_config.yaml" 
 
-    pcd_to_geojson(input_pcd, output_geojson, orglat, orglon)
-
+    pcd_to_geojson(input_pcd, output_geojson, config_file)
